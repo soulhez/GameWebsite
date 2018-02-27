92783f9597884029710e8160b4351adcb4aa369d
unkownY <unkownY@163.com>
2018-02-27 19:30:29 +0800

    .gitignore

--- planeGame/index.js
+++ planeGame/index.js
@@ -7,36 +7,41 @@ window.onload = function(){
   // 开始游戏
   start.onclick = function(){
    document.body.setAttribute("data-stat","start");
-      // 游戏开始
-      
-    mainGame();
+    // 游戏开始
+    main();
   }
 
   // 游戏说明
-  intro.onclick = function(){
-    document.body.setAttribute("data-stat","intro");
+  intro.onclick = function () {
+    document.body.setAttribute("data-stat", "intro");
   }
-  document.getElementsByClassName("closeIntro")[0].onclick = function(){
-    document.body.setAttribute("data-stat","index");
+  document.getElementsByClassName("closeIntro")[0].onclick = function () {
+    document.body.setAttribute("data-stat", "index");
   };
-
   // 退出游戏
-  exit.onclick = function(){
+  exit.onclick = function () {
     window.close();
   }
 }
 
 // 游戏主函数
-let Game = {
-  canvas : document.getElementById("game"),
-  ctx : canvas.getContext("2d"),
-  init : function(){
-  
-  },
-  start : function(){
+function main(){
+  let ctx = init();
+  enermy();
 
-  },
-  end : function(){
+  function init(){
+    let can = document.getElementById("game");
+    let ctx = can.getContext("2d");
+    return ctx;
+  }
 
+  function enermy(){
+    let x = 20;
+    let y = 30;
+    x, y = x + 10, y + 20;
+    ctx.fillStyle = "red";
+    ctx.arc(x, y, 10, 0,Math.PI*2);
+    ctx.fill();
+    window.requestAnimationFrame(enermy);
   }
 }
